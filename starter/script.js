3c3ced66aa1eea4324da76dc3d450a639a4d888b
ArchanaManju <archanahm2021@gmail.com>
2022-01-21 22:34:48 +0000

    added again logic

--- starter/script.js
+++ starter/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 console.log(document.querySelector('.message').textContent);
 const getNumber = function () {
@@ -12,6 +12,12 @@ const getNumber = function () {
     document.querySelector('.message').textContent = '⛔ No number';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     console.log(score);
     if (score > 1) {
@@ -32,3 +38,14 @@ const getNumber = function () {
   }
 };
 document.querySelector('.check').addEventListener('click', getNumber);
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing... ';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
